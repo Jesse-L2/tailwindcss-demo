eca1b11ed7a47a0ee91ca733a608a46b2db494d8
Jesse-L2 <jesse.little@utexas.edu>
2024-05-06 10:14:35 -0500

    Added dynamic navlinks

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,7 @@
 import { headerLogo } from "../assets/images";
 import { hamburger } from "../assets/icons";
+import { navLinks } from "../constants";
+
 const Nav = () => {
   return (
     <header className="padding-x py-8 absolute z-10 w-full">
@@ -7,7 +9,18 @@ const Nav = () => {
         <a href="/">
           <img src={headerLogo} alt="Logo" width={130} height={29} />
         </a>
-        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden"></ul>
+        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <a
+                href={item.href}
+                className="font-montserrat leading-normal text-lg text-slate-gray"
+              >
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
       </nav>
     </header>
   );
