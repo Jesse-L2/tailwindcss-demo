3489b83c2cb9b11bbeaf360685a31814e4c6209c
Jesse-L2 <jesse.little@utexas.edu>
2024-05-07 11:20:42 -0500

    PopularProducts section formatting

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,5 +1,11 @@
+import { products } from "../constants";
+
 const PopularProducts = () => {
-  return ();
+  return (
+    <section id="products" className="max-container max-sm:mt-12">
+      <div></div>
+    </section>
+  );
 };
 
 export default PopularProducts;
